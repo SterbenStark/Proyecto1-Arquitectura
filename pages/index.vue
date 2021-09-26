e4eca97c4c02935924a6395337a5c9e1f7e57127
Milton Gómez <pechex10@gmail.com>
2021-09-25 22:41:33 -0600

    info index

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,13 @@
    <HeaderView />
    <h2 style="margin-top: 15px">Catálogo de Música de Películas</h2>
 	  <p>Este sitio muestra información sobre distintos compositores de música de peliculas a lo largo de la historia, donde
-      también se podran apreciar sus albunes y las peliculas donde tuvo una participación.
-      <br>
-      Se podrán visualizar información de entidades principales tales como: Compositores, Albunes y Peliculas.</p>
+      también se podran apreciar sus albunes y las películas donde tuvo una participación.
+      <br><br>
+      Se podrá visualizar información de entidades principales tales como: Compositores, Albunes y Películas.</p>
     <ul>
-	  <li> <NuxtLink to="/books_index">Books information</NuxtLink></li>
-	  <li> <NuxtLink to="/authors_index">Authors information</NuxtLink></li>
-	  <li> <NuxtLink to="/publishers_index">Publishers information</NuxtLink></li>
+	  <li> <NuxtLink to="/books_index">Información de compositores</NuxtLink></li>
+	  <li> <NuxtLink to="/authors_index">Información de albunes</NuxtLink></li>
+	  <li> <NuxtLink to="/publishers_index">Información de películas</NuxtLink></li>
 	</ul>
    <FooterView />
  </div>
