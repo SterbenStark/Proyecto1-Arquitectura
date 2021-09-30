3ec1646793bde1812c466f12d5128daea29ca1a8
Milton Gómez <pechex10@gmail.com>
2021-09-30 00:50:08 -0600

    pelicula

---
+++ content/peliculas/pelicula_1.md
@@ -0,0 +1,15 @@
+---
+id: pelicula_1
+name: Mi Villano Favorito 2
+direction: Chris Renaud, Sergio Pablos, Pierre Coffin
+country: United States
+year: 2013
+duration: 95 minutos
+genere:
+  - Familiar
+  - Animación
+albumId: album_1
+image: peliculas/pelicula_1.jpg
+---
+
+Despicable Me 2 (titulada Mi villano favorito 2 en Hispanoamérica, y Gru 2: Mi villano favorito en España) es una película infantil animada estrenada el 5 de julio de 2013. Se trata de la secuela de Mi Villano Favorito (2010). Producida por Illumination Entertainment y distribuida por Universal Studios Inc., dirigida por Pierre Coffin y Chris Renaud y escrita por Cinco Paul y Ken Daurio. Steve Carell, Russell Brand y Miranda Cosgrove interpretan de nuevo sus papeles. Kristen Wiig, quien interpretó a Miss Hattie en la primera película, interpreta a la agente Lucy Wilde. Entre los nuevos miembros del elenco se encuentran Benjamin Bratt y Steve Coogan. El estreno de la película fue el 3 de julio de 2013. En México se estrenó el 5 de julio del mismo año.
---
+++ pages/peliculas/_slug.vue
@@ -0,0 +1,38 @@
+<script>
+  export default {
+    async asyncData({ $content, params }) {
+      const pelicula = await $content('peliculas', params.slug).fetch()
+      const album = await $content('albums').where({ id: pelicula.albumId }).only(['name']).fetch()
+      return { album, pelicula }
+    }
+  }
+</script>
+
+<template>
+  <div class="container">
+   <HeaderView />
+   <div class="row">
+     <div class="three columns">
+       <img class="u-max-full-width" :src="'/images/'+pelicula.image">
+     </div>
+     <div class="six columns">
+       <h4>{{pelicula.name}}</h4>
+	   Dirección de la película: {{pelicula.direction}}</br>
+     País: {{pelicula.country}}</br>
+     Año de estreno: {{pelicula.year}}</br>
+     Duración de la película: {{pelicula.duration}}</br>
+	   Generos: {{pelicula.genere}}</br></br>
+	   <b>Description</b></br>
+	    <nuxt-content :document="pelicula" />
+	 </div>
+	 <div class="three columns"></div>
+	   <h5>Album Asociado</h5>
+	   <ul>
+	     <li v-for="album of albums" :key="album.slug">
+	       <NuxtLink :to="{ name: 'albums-slug', params: { slug: album.slug } }">{{album.name}}</NuxtLink>
+	     </li>
+	   </ul>
+   </div>
+   <FooterView />
+ </div>
+</template>
