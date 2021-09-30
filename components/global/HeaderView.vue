173afa000fe409c98565d25ce8819f7b1e2f72ba
Milton Gómez <pechex10@gmail.com>
2021-09-30 00:52:32 -0600

    menu

--- components/global/HeaderView.vue
+++ components/global/HeaderView.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="row"  style="margin-top: 15px">
       <NuxtLink class="three columns button button-primary" to="/">Home</NuxtLink>
-      <NuxtLink class="three columns button button-primary" to="/books_index">Books</NuxtLink>
       <NuxtLink class="three columns button button-primary" to="/compositors_index">Compositores</NuxtLink>
-      <!--<NuxtLink class="three columns button button-primary" to="/publishers_index">Publishers</NuxtLink>-->
       <NuxtLink class="three columns button button-primary" to="/albums_index">Albums</NuxtLink>
-      
+      <NuxtLink class="three columns button button-primary" to="/peliculas_index">Peliculas</NuxtLink>
+      <!--<NuxtLink class="three columns button button-primary" to="/books_index">Books</NuxtLink>-->
+      <!--<NuxtLink class="three columns button button-primary" to="/publishers_index">Publishers</NuxtLink>-->
     </div>
 </template>
